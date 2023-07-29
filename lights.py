2e1ede5f8b700a95a229c95ecfb30fcf11b5efec
Wouter Eerdekens <retuow@protonmail.com>
2023-07-29 16:05:41 +0200

    Cleanup

--- lights.py
+++ lights.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     if response.status != 200:
         raise Exception('Unable to retrieve sunrise/sunset data')
     data = json.loads(response.read().decode('utf-8'))
-    if not data['status'] == 'OK':
+    if data['status'] != 'OK':
         raise Exception('Bad request sent to API')
     sunrise = datetime.strptime(data['results']['sunrise'], '%I:%M:%S %p').time()
     sunset = datetime.strptime(data['results']['sunset'], '%I:%M:%S %p').time()
